16522bbe34fbf5d1274b13e2f6e40be20389c580
Arindam <bis.ips22@gmail.com>
2023-08-05 14:47:32 +0530

    Some improvement in add medicine functionality

--- src/components/store/cart-context.js
+++ src/components/store/cart-context.js
@@ -1,4 +1,5 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
+import axios from "axios";
 
 export const CartContext = createContext();
 
@@ -26,6 +27,11 @@ const cartReducer = (state, action) => {
           return item;
         })
         .filter((item) => item.quantity > 0);
+    case "SET_CART_ITEMS":
+      return action.payload.map((cartItem) => ({
+        ...cartItem,
+        serverGeneratedId: cartItem._id,
+      }));
     default:
       return state;
   }
@@ -35,17 +41,69 @@ export const CartProvider = (props) => {
   const [cart, dispatch] = useReducer(cartReducer, []);
 
   const addToCart = (medicine) => {
-    dispatch({ type: "ADD_TO_CART", payload: medicine });
+    const existingMedicine = cart.find(
+      (item) => item.serverGeneratedId === medicine._id
+    );
+
+    if (existingMedicine) {
+      dispatch({ type: "ADD_TO_CART", payload: medicine });
+    } else {
+      const itemToAdd = {
+        serverGeneratedId: medicine._id,
+        name: medicine.name,
+        description: medicine.description,
+        price: medicine.price,
+        quantity: 1,
+      };
+
+      axios
+        .post(
+          "https://crudcrud.com/api/d7b342f0641e412f861842737268b245/CartStock",
+          itemToAdd
+        )
+        .then((response) => {
+          dispatch({
+            type: "ADD_TO_CART",
+            payload: { ...itemToAdd, serverGeneratedId: response.data._id },
+          });
+        })
+        .catch((error) => {
+          console.error("Error adding medicine to cart:", error);
+        });
+    }
   };
 
   const removeFromCart = (medicineId) => {
-    const cartItem = cart.find((item) => item.id === medicineId);
+    const cartItem = cart.find((item) => item.serverGeneratedId === medicineId);
 
     if (cartItem && cartItem.quantity > 0) {
-      dispatch({ type: "REMOVE_FROM_CART", payload: medicineId });
-      props.updateMedicineQuantity(medicineId, 1); 
+      dispatch({ type: "REMOVE_FROM_CART", payload: cartItem.id });
+
+      const updatedQuantity = cartItem.quantity - 1;
+
+      axios
+        .put(
+          `https://crudcrud.com/api/d7b342f0641e412f861842737268b245/CartStock/${medicineId}`,
+          { quantity: updatedQuantity }
+        )
+        .catch((error) => {
+          console.error("Error updating medicine quantity:", error);
+        });
     }
   };
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://crudcrud.com/api/d7b342f0641e412f861842737268b245/CartStock"
+      )
+      .then((response) => {
+        dispatch({ type: "SET_CART_ITEMS", payload: response.data });
+      })
+      .catch((error) => {
+        console.error("Error fetching cart items:", error);
+      });
+  }, []);
 
   return (
     <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
